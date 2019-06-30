18e0b7e9b66b6b5c982f6bd04502ffd6483d12b2
Ivan Vozniuk <iivozniuk@gmail.com>
2019-07-01 00:35:58 +0300

    fixed to-do list

--- src/components/ToDo/components/Task.jsx
+++ src/components/ToDo/components/Task.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Task = ({name, isCompleted, id, toggleTask, deleteTask}) => (
 	<div className="ui segment purple d-flex justify-content-between" checked={isCompleted}>
 		<div onClick={() => toggleTask(id)}>
-			<i className={isCompleted ? "icon check circle outline" : "icon circle outline"}></i>
-			<span className={isCompleted ? "task-name" : "task-name task-name_disabled"}>{name}</span>
+			<i className={isCompleted ? "icon check circle outline icon_disabled" : "icon circle outline"}></i>
+			<span className={isCompleted ? "task-name task-name_disabled" : "task-name"}>{name}</span>
 		</div>
 		<i onClick={() => deleteTask(id)}className="icon x"></i>
 	</div>
